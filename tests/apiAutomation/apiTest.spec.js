f8c33185a58648f9d3466fb5fbec374662cad599
Siddhant2127 <siddhant2127.be21@chitkara.edu.in>
2025-07-13 16:09:40 +0530

    Added API Automation test script

---
+++ tests/apiAutomation/apiTest.spec.js
@@ -0,0 +1,50 @@
+import { test, expect } from "@playwright/test";
+import fs from "fs";
+
+const baseURL = "https://reqres.in/api";
+
+const userDetails = {
+  name: "Jack",
+  job: "leader",
+};
+
+test("API Automation", async ({ request }) => {
+  //1. Create a new user, validate the http status code and retrieve user id
+  const postResponse = await request.post(`${baseURL}/users`, {
+    headers: {
+      "x-api-key": "reqres-free-v1",
+    },
+    data: userDetails,
+  });
+
+  expect(postResponse.status()).toBe(201);
+  const postResponseJson = await postResponse.json();
+  const userId = postResponseJson.id;
+  console.log("User ID:", userId);
+
+  //2. Get the created user details and validate the same
+  const getRes = await request.get(`${baseURL}/users/${userId}`, {
+    headers: {
+      "x-api-key": "reqres-free-v1",
+    },
+  });
+  expect([200, 404]).toContain(getRes.status());
+  if (getRes.status() == 200) {
+    const fetchedData = await getRes.json();
+    expect(fetchedData.name).toBe(userDetails.name);
+    expect(fetchedData.job).toBe(userDetails.job);
+  }
+
+  // 3. Update User
+  const updateRes = await request.put(`${baseURL}/users/${userId}`, {
+    headers: {
+      "x-api-key": "reqres-free-v1",
+    },
+    data: {
+      name: "Jane",
+    },
+  });
+  expect(updateRes.status()).toBe(200);
+  const updateData = await updateRes.json();
+  expect(updateData.name).toBe("Jane");
+});
--- tests/bookStoreApplication/uiTest.spec.js
+++ tests/bookStoreApplication/uiTest.spec.js
@@ -1,7 +1,8 @@
 import { test, expect } from "@playwright/test";
-import fs from 'fs';
+import fs from "fs";
 
 test("UI Automation", async ({ page }) => {
+  test.setTimeout(60000);
   await page.goto("https://demoqa.com/");
   // Navigate
   await page.getByRole("heading", { name: "Book Store Application" }).click();
@@ -9,7 +10,9 @@ test("UI Automation", async ({ page }) => {
   // Login a user
   await page.getByRole("button", { name: "Login" }).click();
   await page.getByRole("textbox", { name: "UserName" }).fill(process.env.USER);
-  await page.getByRole("textbox", { name: "Password" }).fill(process.env.PASSWORD);
+  await page
+    .getByRole("textbox", { name: "Password" })
+    .fill(process.env.PASSWORD);
   await page.getByRole("button", { name: "Login" }).click();
   // Validate the username and log out button to be visible
   await expect(page.locator("#userName-value")).toContainText("tempuser234");
@@ -24,13 +27,13 @@ test("UI Automation", async ({ page }) => {
     page.getByRole("link", { name: "Learning JavaScript Design Patterns" })
   ).toBeVisible({ timeout: 10000 });
 
-  const bookTitle =  await page.locator(".rt-td").nth(1).textContent();
+  const bookTitle = await page.locator(".rt-td").nth(1).textContent();
   const bookAuthor = await page.locator(".rt-td").nth(2).textContent();
   const bookPublisher = await page.locator(".rt-td").nth(3).textContent();
   let bookDetailsData = `"Title","Author","Publisher"\n`;
   bookDetailsData += `${bookTitle},${bookAuthor},${bookPublisher}\n`;
-  fs.writeFileSync("BookDetails.csv",bookDetailsData)
+  fs.writeFileSync("BookDetails.csv", bookDetailsData);
   console.log("Book Details successfully added in `BookDetails.csv`");
 
-  await page.getByRole('button',{name:'Log out'}).click();
+  await page.getByRole("button", { name: "Log out" }).click();
 });
